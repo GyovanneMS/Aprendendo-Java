9831a0023573bc18532e39d951b3cc880d23869a
GYOVANNE MARTINS SABARÁ <22102360@CFP127ALUNO.LOCAL>
2022-02-23 08:46:49 -0300

    Atualização de código

--- Mensagem.java
+++ Mensagem.java
@@ -4,13 +4,21 @@ class Mensagem{
     public static void main(String args[]) {
         JOptionPane jop = new JOptionPane();
 
-        int anoNascimento = jop.showConfirmDialog(null, "Qual o ano do seu nasimento?");
-
-        //String name = jop.showInputDialog(null,"What's your name?");
+        String name = jop.showInputDialog(null,"What's your name?");
+        String anoNascimento = jop.showInputDialog(null, "Qual é o ano do seu nascimento?");
+        int anoN = Integer.parseInt(anoNascimento);
+        int idade = 2022 - anoN;
+        jop.showMessageDialog(null, "Sea Bienvenido(a) " + name + ", com " +  idade + " de idade" ); //name Idade
+        
         //int AnoN = jop.showInputDialog(null,"Em que ano você nasceu?");
         //int AnoHj = jop.showInputDialog(null, "Em que ano estamos?");
-        //int Idade = AnoHj - AnoN;
-        //jop.showMessageDialog(null, "Sea Bienvenido(a) pessoa com idade" + AnoN ); //name Idade
+        //
 
     }
 }
+
+/* 
+git add . 
+git commit -m "Atualização de código"
+git push
+*/
