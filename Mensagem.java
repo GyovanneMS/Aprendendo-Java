e7a24d735a30da0b8ee7c59aca5a079c91a16289
GYOVANNE MARTINS SABARÁ <22102360@CFP127ALUNO.LOCAL>
2022-02-23 09:38:38 -0300

    Atualização de código

--- Mensagem.java
+++ Mensagem.java
@@ -6,8 +6,13 @@ class Mensagem{
 
         String name = jop.showInputDialog(null,"What's your name?");
         int anoNascimento = Integer.parseInt(jop.showInputDialog(null, "Qual é o ano do seu nascimento?"));
+        double altura = Double.parseDouble(jop.showInputDialog(null, "Qual a sua altura?"));
+        int peso = Integer.parseInt(jop.showInputDialog(null, "Qual é o seu peso aproximadamente? (sem vírgula, como por exemplo 90)" ));
+        double peso2 = Math.pow(2, peso);
+        double iMC = altura / peso2;
         int idade = 2022 - anoNascimento;
         jop.showMessageDialog(null, "Sea Bienvenido(a) " + name + ", com " +  idade + " anos de idade!!" );
+        jop.showMessageDialog(null, "Seu IMC é igual a " + iMC + "!" );
         
 
     }
